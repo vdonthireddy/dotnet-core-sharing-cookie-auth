48dad69325a6f218717b70997d22d598031e9aba
Vijaya Donthireddy <donmac@vijayas-mbp.lan>
2020-05-02 23:03:14 -0700

    first commit

---
+++ sharing-cookie-auth/App1/Startup.cs
@@ -0,0 +1,99 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.DataProtection;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace App1
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+                .AddCookie(config =>
+                {
+                    config.Cookie.Name = "vj_auth_local";
+                    config.Cookie.Domain = "localhost";
+                    config.Events = new CookieAuthenticationEvents()
+                    {
+                        OnRedirectToLogin = (context) =>
+                        {
+                            context.HttpContext.Response.Redirect("http://localhost:5000/auth/login");
+                            return Task.CompletedTask;
+                        }
+                    };
+                });
+
+            services.AddDataProtection()
+                .PersistKeysToFileSystem(GetKyRingDirectoryInfo())
+                .SetApplicationName("SharedCookieApp");
+        }
+
+        private DirectoryInfo GetKyRingDirectoryInfo()
+        {
+            string applicationBasePath = System.AppContext.BaseDirectory;
+            DirectoryInfo directoryInof = new DirectoryInfo(applicationBasePath);
+            string keyRingPath = Configuration.GetSection("AppKeys").GetValue<string>("keyRingPath");
+            do
+            {
+                directoryInof = directoryInof.Parent;
+
+                DirectoryInfo keyRingDirectoryInfo = new DirectoryInfo($"{directoryInof.FullName}{keyRingPath}");
+                if (keyRingDirectoryInfo.Exists)
+                {
+                    return keyRingDirectoryInfo;
+                }
+
+            }
+            while (directoryInof.Parent != null);
+            throw new Exception($"key ring path not found");
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
---
+++ sharing-cookie-auth/auth/Controllers/AuthController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace auth.Controllers
+{
+    public class AuthController : Controller
+    {
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(string name, string returnUri = null)
+        {
+            if (name == "Vijay")
+            {
+                var claims = new List<Claim>()
+                {
+                    new Claim(ClaimTypes.Name, "vdonthireddy@gmail.com"),
+                    new Claim("FullName", "Vijay Donthireddy"),
+                    new Claim(ClaimTypes.Role, "Administrator"),
+                };
+                var authProperties = new AuthenticationProperties
+                {
+                    IsPersistent = true,
+                    RedirectUri = returnUri,
+                    ExpiresUtc = DateTime.UtcNow.AddMinutes(20)
+                };
+
+                var claimsIdentity = new ClaimsIdentity(
+                    claims, CookieAuthenticationDefaults.AuthenticationScheme);
+                HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity),
+                    authProperties);
+                return RedirectToAction("Privacy", "Home");
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        public void Logout()
+        {
+            HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+        }
+    }
+}
